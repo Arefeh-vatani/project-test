e8cd5db27ac8579ded799633026b91ac6ed21c92
Arefeh-vatani <v.arefeh@yahoo.com>
2022-11-16 20:48:38 +0330

    Mohasebe sen

--- main.cs
+++ main.cs
@@ -1,7 +1,16 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    int days;
+    days = Convert.ToInt32(Console.ReadLine());
+
+    //years
+    Console.WriteLine("Y: " + days/365);
+    days = days%365;
+    //month
+    Console.WriteLine("M: " + days/30);
+    days = days%30;
+    //day
+    Console.WriteLine("D: " + days);
   }
 }
-//test
