271157af1fea4f9417b9b3893968c7cbebd5ab80
Arefeh-vatani <v.arefeh@yahoo.com>
2022-11-23 12:21:31 +0330

    years

--- main.cs
+++ main.cs
@@ -2,15 +2,16 @@ using System;
 class HelloWorld {
   static void Main() {
     int days;
-    days = Convert.ToInt32(Console.ReadLine());
+    days = Convert.ToInt32(Console.ReadLine());//int days = console.ReadLine()
+    //int days = Convert.ToInt32(days)
 
     //years
-    Console.WriteLine("Y: " + days/365);
+    Console.WriteLine("Years : " + days/365);
     days = days%365;
     //month
-    Console.WriteLine("M: " + days/30);
+    Console.WriteLine("Months : " + days/30);
     days = days%30;
     //dayy
-    Console.WriteLine("D: " + days);
+    Console.WriteLine("Days : " + days);
   }
 }
